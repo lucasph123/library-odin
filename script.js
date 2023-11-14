6b59fdfb8caf5bb1683747a7293a98c17b654688
Lucas <41449194+lucasph123@users.noreply.github.com>
2023-11-14 14:48:28 -0300

    added insertion of the book in the content, added animaion for show bescription behind the book added button for remove book (in work) added book working in array

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 
-const myLibrary = new Object();
-
+const libArray = [];
 const modal = document.querySelector('.modal-form');
 const btnOpen = document.querySelector('.btn-open');
 const btnClose = document.querySelector('.btn-close');
 const libContent = document.querySelector('.library-content');
+const btnSubmit = document.querySelector('#btn-submit');
 
 
-
+btnOpen.addEventListener('click', () => modal.showModal());
 btnClose.addEventListener('click', () => modal.close());
-btnOpen.addEventListener('click', function addBookToLibrary()
-{
-    modal.showModal();
 
+btnSubmit.addEventListener('click', function addBookToLibrary()
+{
+    const subTitle = document.querySelector('#title').value;
+    const subDesc = document.querySelector('#description').value;
+    const subAuthor = document.querySelector('#author').value;
+    const subPages = document.querySelector('#pages').value;
+    console.log(subDesc);
+    const myLibrary = new SaveBookInLibrary(subTitle,subDesc,subAuthor,subPages);
+    createVisualBook(subTitle,subDesc,subAuthor,subPages);
+    libArray.push(myLibrary);
+      
+ 
 });
 
-
-
-
-function displayLibrary()
+function SaveBookInLibrary(subTitle,subDesc,subAuthor,subPages)
 {
-    for (const key in myLibrary) {
-        console.log(myLibrary.book)
 
-    }
+    this.title = subTitle;
+    this.desc = subDesc;
+    this.author = subAuthor
+    this.pages = subPages;
+
 }
 
-function createVisualBook(title)
+
+function createVisualBook(subTitle,subDesc,subAuthor,subPages)
 {
     
 
@@ -35,28 +44,45 @@ function createVisualBook(title)
     const descDiv = document.createElement('div');
     const authorDiv = document.createElement('div');
     const pagesDiv = document.createElement('div');
+    const btnDel = document.createElement('button');
+    btnDel.addEventListener('click', () => deleteBook(titleDiv.textContent));
 
     bookDiv.classList.add('book');
     titleDiv.classList.add('title');
     descDiv.classList.add('description');
     authorDiv.classList.add('author')
     pagesDiv.classList.add('pages')
+    btnDel.classList.add('btn-del');
 
-    titleDiv.textContent= title;
-    descDiv.textContent='Description';
-    authorDiv.textContent='title';
-    pagesDiv.textContent='title';
+    titleDiv.textContent= subTitle;
+    descDiv.textContent=subDesc;
+    authorDiv.textContent=subAuthor;
+    pagesDiv.textContent=subPages;
+    btnDel.textContent = 'X';
 
     libContent.appendChild(bookDiv);
     bookDiv.appendChild(titleDiv);
     bookDiv.appendChild(descDiv);
     bookDiv.appendChild(authorDiv);
     bookDiv.appendChild(pagesDiv);
+    bookDiv.appendChild(btnDel);
 
 }
 
-
-function showBookForm()
+function deleteBook(titleDiv)
 {
+
+    const find = libArray.findIndex((x) => x.title === titleDiv)
+    console.log(find);
+     if (find!== -1) libArray.splice(find,1);
+    console.log(libArray);
+   // libContent.removeChild(node);
+    updateVisualBook();
+
+}
+
+function updateVisualBook(){
+
+
 
 }
